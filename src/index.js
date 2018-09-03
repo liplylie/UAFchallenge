0c601e01b894e26e2ed2f92f5f3bcf63f11cba3f
Jordan Daniels <decentpianist@gmail.com>
2018-09-02 18:25:41 -0700

    initial set up

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "../client/Components/App.js"
+
+ReactDOM.render(<App />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { join, resolve } = require("path");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+    template: resolve(__dirname, 'src', 'index.html'),
+    filename: "./index.html"
+});
+
+module.exports = {
+    entry: join(__dirname, 'src', 'index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [htmlWebpackPlugin]
+};
